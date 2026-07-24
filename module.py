0de7ddcd397771665022ac72a09d9d219626b85a
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-24 06:07:38 +0000

    Lesson6

--- module.py
+++ module.py
@@ -1,19 +1,54 @@
-# Module is a File containing definition and statements.
-# Module is defined by its name. If you want to use any module you need to know the name.
-# To use the Module you need to call it by 'import' followed by the module name. (eg,. import math)
+# A module is a Python (.py) file that contains reusable code,
+    # such as functions, classes, variables, and statements.
+# A module is a Python file that contains reusable code and can
+    # be imported into another Python program using the import statement.
 
-# Example how to use a Module:
-import math as m # Aliasing
+# Why use modules?
+# - Reuse code
+# - Organize programs
+# - Make code easier to maintain
+# - Access built-in and third-party libraries
 
+# Types of Modules:
 
-def sin(x):
-    if 2 * x == pi:
-        return 0.99999999
-    else:
-        return None
+# 1. Built-in Modules
+    # Included with Python
+    # Examples: math, random, os, datetime, sys
 
+# 2. User-Defined Modules
+    # Modules created by the programmer
+    # Example: calculator.py
 
-pi = 3.14
+# 3. Third-Party Modules
+    # Created by other developers
+    # Installed using: pip install <module_name>
+    # Examples: requests, numpy, pandas, matplotlib
 
-print(sin(pi/2))
-print(m.sin(m.pi/2))
+# Ways to Import Modules:
+# - import math
+# - from math import sqrt
+# - import math as m
+# - from math import *   # Not recommended
+
+# Example of Simple Calculator using the built-in math module
+import math
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b != 0:
+            return a / b
+        return "Cannot divide by zero."
+
+    def square_root(self, a):
+        if a >= 0:
+            return math.sqrt(a)
+        return "Square root is not defined for negative numbers."

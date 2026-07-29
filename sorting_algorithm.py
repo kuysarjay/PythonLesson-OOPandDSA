f3a159ba0c36cbee5909be1919ea80a00bce0bf2
Nelson Sto. Domingo <arjaystodomingo07@gmail.com>
2026-07-29 21:49:43 +0000

    Lesson22

---
+++ sorting_algorithm.py
@@ -0,0 +1,14 @@
+# Sorting algorithms are used to arrange the elements of a data structure, 
+    # such as an array, list, or tree, in a particular order, typically in ascending or descending order.
+# These algorithms are used for organizing data, which enables more efficient searching, 
+    # merging, and other operations.
+    
+# Example of Sorting Algorithm:
+nums = [5, 3, 8, 1]
+
+# In-place sort
+nums.sort()
+print(nums)       
+
+# New sorted list (descending)
+print(sorted(nums, reverse=True))
